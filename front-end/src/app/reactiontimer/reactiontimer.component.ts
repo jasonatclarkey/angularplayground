4787ffda06aadbba51e67c830e161f81572e2d9c
Jason Clarke <jason.clarke@papercut.com>
2024-04-16 16:42:34 +1000

    Added reaction timer.

--- front-end/src/app/reactiontimer/reactiontimer.component.ts
+++ front-end/src/app/reactiontimer/reactiontimer.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reactiontimer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reactiontimer.component.html',
   styleUrl: './reactiontimer.component.css'
 })
 export class ReactiontimerComponent {
 
+  isTimerRunning: boolean = false;
+  isRed: boolean = false;
+  startTime: number = 0;
+  score: number = 0.00;
+
+  startTimer() {
+    this.isTimerRunning = true;
+    this.isRed = false;
+    const randomTime = (Math.random() * 4000) + 1000;
+    setTimeout(() => {
+      this.isRed = true;
+      this.startTime = performance.now();
+    }, randomTime);
+  }
+
+  calculateScore() {
+    if (this.isRed) {
+      const endTime = performance.now();
+      this.score = (endTime - this.startTime) / 1000;
+      this.isTimerRunning = false;
+      this.isRed = false;
+    }
+  }
 }
